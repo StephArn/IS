ae8a6dfcc0f7e8894fdfaf6b713c0b162ad64ee1
StephArn <stefana.arina@gmail.com>
2022-01-30 03:26:59 +0200

    partial ambulance detection - intervals

--- complete/src/main/java/com/example/handlingformsubmission/Greeting.java
+++ complete/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -2,23 +2,47 @@ package com.example.handlingformsubmission;
 
 public class Greeting {
 
-	private long id;
-	private String content;
+	private double mfcc1, mfcc2, mfcc3;
 
-	public long getId() {
-		return id;
+//	public Greeting(double mfcc1, double mfcc2, double mfcc3) {
+//		this.mfcc1 = mfcc1;
+//		this.mfcc2 = mfcc2;
+//		this.mfcc3 = mfcc3;
+//	}
+//
+//	public Greeting() {
+//
+//	}
+
+	public Boolean detectAmbulance(){
+		boolean first = (0.08 <= mfcc1) && (mfcc1 <= 0.13);
+		boolean second = (-188.05 <= mfcc2) && (mfcc2 <= -110.32);
+		boolean third = (96.04 <= mfcc3) && (mfcc3 <= 149.29);
+
+		return first && second && third;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	public double getMfcc1() {
+		return mfcc1;
 	}
 
-	public String getContent() {
-		return content;
+	public void setMfcc1(double mfcc1) {
+		this.mfcc1 = mfcc1;
 	}
 
-	public void setContent(String content) {
-		this.content = content;
+	public double getMfcc2() {
+		return mfcc2;
 	}
 
+	public void setMfcc2(double mfcc2) {
+		this.mfcc2 = mfcc2;
+	}
+
+	public double getMfcc3() {
+		return mfcc3;
+	}
+
+	public void setMfcc3(double mfcc3) {
+		this.mfcc3 = mfcc3;
+	}
 }
